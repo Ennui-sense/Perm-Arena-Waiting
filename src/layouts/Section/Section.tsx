20b41b26b8f2b3450e08f1b5ce049bb8e3b4f75d
Anatoliy Shreder <anatoliy.shreder@gmail.com>
2026-06-28 19:30:38 +0500

    redesign

--- src/layouts/Section/Section.tsx
+++ src/layouts/Section/Section.tsx
@@ -1,21 +1,36 @@
 import clsx from "clsx";
 import "./Section.scss";
-import type { JSX, ReactNode } from "react";
+import type { ReactNode } from "react";
 
 interface ISection {
   sectionName: string;
   description?: string;
-  title: JSX.Element;
+  title: string;
+  subtitle: string;
   children: ReactNode;
+  rightContent?: string;
 }
 
-const Section = ({ sectionName, description, title, children }: ISection) => {
+const Section = ({
+  sectionName,
+  description,
+  title,
+  subtitle,
+  children,
+  rightContent,
+}: ISection) => {
   return (
     <section className="section">
       <div className="section__innner container">
         <header className="section__header">
-          <h2 className="section__title">{title}</h2>
+          <div className="section__header-info">
+            <h2 className="section__title">{title}</h2>
+            <p className="section__subtitle">{subtitle}</p>
+          </div>
           {description && <p className="section__description">{description}</p>}
+          {rightContent && (
+            <p className="section__right-content">{rightContent}</p>
+          )}
         </header>
         <div className={clsx("section__body", sectionName)}>{children}</div>
       </div>
--- src/sections/Readiness/Readiness.tsx
+++ src/sections/Readiness/Readiness.tsx
@@ -1,67 +1,78 @@
 import "./Readiness.scss";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
 
 import Section from "~/layouts/Section/Section";
 
 import VisualizationIcon from "~/assets/icons/visualization.svg?react";
+import { useInView } from "react-intersection-observer";
 
-const READINESS_PERCENT = 67;
+const READINESS_PERCENT = 32;
 
 const clampPercent = (value: number) => Math.min(Math.max(value, 0), 100);
 
 const Readiness = () => {
-  const visualizationRef = useRef<HTMLDivElement>(null);
   const readinessPercent = clampPercent(READINESS_PERCENT);
+  const [currentPercent, setCurrentPercent] = useState(0);
+  const { ref, inView } = useInView({ triggerOnce: true });
 
   useEffect(() => {
-    const visibleFigures =
-      visualizationRef.current?.querySelectorAll<SVGPathElement>(
-        "svg > path[stroke]",
-      );
-
-    if (!visibleFigures?.length) {
+    if (!inView) {
       return;
     }
 
-    const filledFiguresCount = Math.round(
-      (visibleFigures.length * readinessPercent) / 100,
-    );
+    const intervalId = setInterval(() => {
+      setCurrentPercent((currentValue) => {
+        if (currentValue >= readinessPercent) {
+          clearInterval(intervalId);
+          return readinessPercent;
+        }
 
-    visibleFigures.forEach((figure, index) => {
-      figure.style.fill = index < filledFiguresCount ? "#fff" : "transparent";
-    });
-  }, [readinessPercent]);
+        return currentValue + 1;
+      });
+    }, 20);
+
+    return () => clearInterval(intervalId);
+  }, [inView, readinessPercent]);
 
   return (
     <Section
       sectionName="readiness"
-      title={
-        <>
-          Готовность
-          <br />
-          проекта{" "}
-        </>
-      }
+      title="Ход строительства"
+      subtitle="Готовность проекта"
+      rightContent={`${currentPercent}%`}
     >
-      <div className="readiness__inner">
-        <div className="readiness__visualization" ref={visualizationRef}>
-          <p className="readiness__visualization-value">{readinessPercent} %</p>
-          <VisualizationIcon
-            className="readiness__visualization-icon"
-            aria-hidden="true"
-          />
+      <div className="readiness__inner" ref={ref}>
+        <div className="readiness__visualization">
+          <VisualizationIcon className="readiness__visualization-icon readiness__visualization-icon--outline" />
+          <div
+            className="readiness__visualization-fill"
+            style={{ height: `${currentPercent}%` }}
+          >
+            <VisualizationIcon className="readiness__visualization-icon readiness__visualization-icon--fill" />
+          </div>
         </div>
-
-        <div className="readiness__text">
-          Следите за тем, как главная стройка Прикамья бретает реальные черты.
-          Наглядный прогресс и отчет о выполненных этапах — от фундамента до
-          монтажа уникальных конструкций Следите за тем, как главная стройка
-          Прикамья обретает реальные черты. Наглядный прогресс и отчет о
-          выполненных этапах — от фундамента до монтажа уникальных конструкций
-          Следите за тем, как главная стройка Прикамья обретает реальные черты.
-          Наглядный прогресс и отчет о выполненных этапах — от фундамента до
-          монтажа уникальных конструкций
+        <div className="readiness__body">
+          <div className="readiness__info">
+            <h3 className="readiness__info-title">Что делаем</h3>
+            <p className="readiness___info-text">
+              Уже начали строительство покрытия — идет укладка профилированного
+              листа и мягкой кровли. В работе — новые участки обходной галереи,
+              внутри здания подрядчики выполняют работы по устройству сетей
+              отопления, электро- и водоснабжения, а в цокольном техническом
+              этаже уже начали первые помещения готовить к отделке
+            </p>
+          </div>
+          <div className="readiness__info">
+            <h3 className="readiness__info-title">Что сделали</h3>
+            <p className="readiness___info-text">
+              Завершается установка металлоконструкций кровли: на сегодня
+              осталось поставить всего шесть ферм из 31 на кровле основной
+              арены, и скоро приступим к сборке кровли тренировочной арены (там
+              16 ферм). Все конструкции будут смонтированы до конца августа, а
+              параллельно с этим специалисты проведут монтаж кровли.
+            </p>
+          </div>
         </div>
       </div>
     </Section>
